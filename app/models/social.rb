b6d51785c957f039cd29b6d59af18323d438d7ac
Emma Navarro <elnavarro55@gmail.com>
2021-02-26 17:31:34 -0500

    Nested form working

--- app/models/social.rb
+++ app/models/social.rb
@@ -1,3 +1,8 @@
 class Social < ApplicationRecord
   belongs_to :user, optional: true
+
+  validates :social_network_name,
+            presence: true
+  validates :profile_link,
+            presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   has_many :socials, dependent: :destroy
   accepts_nested_attributes_for :socials, allow_destroy: true, reject_if: proc { |att| att['profile_link'].blank? }
+
+  validates :first_name,
+            presence: true
+  validates :last_name,
+            presence: true
+  validates :email,
+            presence: true,
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+                      message: 'must be a valid email address' },
+            length: { maximum: 100 }
+  validates :bio,
+            presence: true,
+            length: { minimum: 30, maximum: 350 }
 end
