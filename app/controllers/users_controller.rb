d3a45db4826a001e29949409ecc250133b10b669
Emma Navarro <elnavarro55@gmail.com>
2021-03-03 16:55:35 -0500

    Implement devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,30 +17,24 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      redirect_to @user, notice: 'User was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully deleted.' }
-    end
+    redirect_to users_url, notice: 'User was successfully deleted.'
   end
 
   private
@@ -58,9 +52,4 @@ class UsersController < ApplicationController
               :bio, :image,
               socials_attributes: %i[id social_network_name profile_link _destroy])
   end
-
-  # def image_type_confirmation
-  #   accepted_types = ['image/png', 'image/jpeg']
-  #   if image.attached? && 
-  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,18 +9,25 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :socials, allow_destroy: true,
                                           reject_if: proc { |att| att['profile_link'].blank? }
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true,
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
-                              message: 'must be a valid email address' },
-                    length: { maximum: 100 }
-  validates :bio, presence: true,
-                  length: { minimum: 30, maximum: 350 }
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  # validates :email, presence: true,
+  #                   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+  #                             message: 'must be a valid email address' },
+  #                   length: { maximum: 100 }
+  # validates :bio, presence: true,
+  #                 length: { minimum: 30, maximum: 350 }
 
   has_one_attached :image do |attachable|
-    attachable.variant :thumbnail, resize_to_fill: '100x100'
+    attachable.variant :thumb, resize: '100x100'
   end
 
-  validates :image, presence: true
+  # validate :image_type_confirmation
+
+  # private
+
+  # def image_type_confirmation
+  #   accepted_types = ['image/png', 'image/jpeg']
+  #   errors.add(:image, 'must be a PNG or JPEG file.') unless !image.attached? && image.content_type.in?(accepted_types)
+  # end
 end
